7e5f24f94eafa1ce6542e278124ec318086ac9db
pugmarx <naqi.rizvi@gmail.com>
2017-07-20 19:51:40 +0000

    wrapup commit

--- legacy-scripts/test/runLocalJob.sh
+++ legacy-scripts/test/runLocalJob.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #hdfs dfs -mkdir -p airline/stage/$1
-for c in {1999..2008}
+for c in {2008..2008}
 do
+	hdfs dfs -rm -r airline/test_stage/$c
 	hadoop jar hadoop-streaming-*.jar \
 	  -D mapreduce.job.reduces=0 \
 	  -D mapreduce.map.output.key.field.separator=',' \
 	  -D mapreduce.partition.keypartitioner.options=-k1,2 \
 	  -input airline/raw/$c \
-	  -output airline/stage/$c \
+	  -output airline/test_stage/$c \
 	  -mapper myMapper.sh \
-	  -file /home/ubuntu/fromlocal/scripts/myMapper.sh
+	  -file /home/ubuntu/ccproj/ccproject/legacy-scripts/test/myMapper.sh
 
-	hdfs dfs -test -e airline/stage/$c/_SUCCESS
+	hdfs dfs -test -e airline/test_stage/$c/_SUCCESS
 	OUT=$?
 		if [ $OUT -eq 0 ];then
 		echo "============ Job Succeeded ====="
-		hdfs dfs -rm -r airline/raw/$c
+		#hdfs dfs -rm -r airline/raw/$c
 	else
 		echo "========== Job FAILURE!!! ========="
 		exit 1
