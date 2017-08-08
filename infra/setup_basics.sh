3e2e62d4228754e9fb69b91122dd32b812c353ee
pugmarx <naqi.rizvi@gmail.com>
2017-08-09 02:08:13 +0530

    add install command for scala

--- infra/setup_basics.sh
+++ infra/setup_basics.sh
@@ -6,3 +6,6 @@ sudo service ntp start
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
+
+# Install scala
+sudo apt-get install -y scala
