89258963d366c566d4137dd49494f456359baee2
pugmarx <naqi.rizvi@gmail.com>
2017-08-09 19:56:14 +0000

    added python

--- infra/setup_basics.sh
+++ infra/setup_basics.sh
@@ -2,10 +2,20 @@
 sudo apt-get install -y ntp
 sudo service ntp start
 
+# Install utils
+sudo apt-get install -y zip
+
 # Install Java
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
 
+
+# Install maven
+# sudo apt-get install -y maven
+
 # Install scala
 sudo apt-get install -y scala
+
+# Install python
+sudo apt-get install -y python
