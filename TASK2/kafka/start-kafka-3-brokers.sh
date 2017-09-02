de32f0a8b4132b342ec5067d028e50edc111c084
pugmarx <naqi.rizvi@gmail.com>
2017-09-02 09:48:34 +0000

    adding kafka scripts

---
+++ TASK2/kafka/start-kafka-3-brokers.sh
@@ -0,0 +1,3 @@
+kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
+kafka-server-start.sh -daemon $KAFKA_HOME/config/server-1.properties
+kafka-server-start.sh -daemon $KAFKA_HOME/config/server-2.properties
---
+++ TASK2/kafka/stop-kafka-3-brokers.sh
@@ -0,0 +1,3 @@
+kafka-server-stop.sh $KAFKA_HOME/config/server.properties &
+kafka-server-stop.sh $KAFKA_HOME/config/server-1.properties &
+kafka-server-stop.sh $KAFKA_HOME/config/server-2.properties &
